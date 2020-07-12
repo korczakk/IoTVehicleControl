c9e474524e643ed5dde6d40ece6beab006d4a1bf
kamil.korczak <kamil.korczak@gmail.com>
2020-07-12 23:00:12 +0200

    Fix for URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  vehicleControlHubUrl: 'http://192.168.1.92:5000/signalr/vehiclecontrol',
+  vehicleControlHubUrl: 'http://192.168.1.90:5000/signalr/vehiclecontrol',
   vehicleControlUrl: 'http://192.168.1.90:5000'
 };
